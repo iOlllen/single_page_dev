ae6ea2a164f60e6a59abf72bbbafc8b3e3242c28
Mikołaj <zelazny.n@gmail.com>
2018-08-20 14:21:40 +0200

    Commit z zawartoscia projektu single_page

---
+++ js/app.js
@@ -0,0 +1,7 @@
+
+particlesJS.load('particles-js', 
+  'https://cdn.jsdelivr.net/npm/particles.js@2.0.0/demo/particles.json', function() {
+  console.log('particles-js loaded')
+});
+
+
---
+++ js/custom_owl.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+	$('.owl-carousel').owlCarousel({
+	    center:true,
+	    loop:true,
+	    margin:40,
+	    responsive:{
+	        0:{
+	            items:1
+	        },
+	        600:{
+	            items:3
+	        },
+	        1000:{
+	            items:5
+	        }
+	    }
+	})
+});
